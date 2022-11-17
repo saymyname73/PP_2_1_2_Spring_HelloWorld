e64bcdb904ba6dabdfee140c7e22490f9bda32fa
saymyname73 <skontrol74@mail.ru>
2022-11-17 21:02:50 +0500

    complit quest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,9 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = applicationContext.getBean(Cat.class);
     }
 }
